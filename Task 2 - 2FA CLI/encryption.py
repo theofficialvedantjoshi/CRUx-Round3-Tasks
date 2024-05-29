8b4c4dd13086d3cd76ef4561f5cadd1c16ebbaa3
Vedant <76871277+theofficialvedantjoshi@users.noreply.github.com>
2024-05-29 12:27:39 +0530

    feat: added encyption to sqlite3 db

---
+++ Task 2 - 2FA CLI/encryption.py
@@ -0,0 +1,28 @@
+from cryptography.fernet import Fernet
+
+
+def encrypt_db():
+    key = Fernet.generate_key()
+    with open("Task 2 - 2FA CLI\\temp\key.key", "wb") as key_file:
+        key_file.write(key)
+    with open("Task 2 - 2FA CLI\\temp\key.key", "rb") as key_file:
+        key = key_file.read()
+    fernet = Fernet(key)
+    with open("Task 2 - 2FA CLI\data\\totp.db", "rb") as file:
+        file_data = file.read()
+    encrypted_data = fernet.encrypt(file_data)
+    with open("totp.db", "wb") as file:
+        file.write(encrypted_data)
+    print("Database encrypted successfully")
+
+
+def decrypt_db():
+    with open("Task 2 - 2FA CLI\\temp\key.key", "rb") as key_file:
+        key = key_file.read()
+    fernet = Fernet(key)
+    with open("Task 2 - 2FA CLI\data\\totp.db", "rb") as file:
+        encrypted_data = file.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
+    with open("totp.db", "wb") as file:
+        file.write(decrypted_data)
+    print("Database decrypted successfully")
