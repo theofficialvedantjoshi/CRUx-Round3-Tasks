67685ce28993a651093286f30bef82bd58c98137
Vedant <76871277+theofficialvedantjoshi@users.noreply.github.com>
2024-05-29 17:18:49 +0530

    fix: db file path

--- Task 2 - 2FA CLI/encryption.py
+++ Task 2 - 2FA CLI/encryption.py
@@ -11,7 +11,7 @@ def encrypt_db():
     with open("Task 2 - 2FA CLI\data\\totp.db", "rb") as file:
         file_data = file.read()
     encrypted_data = fernet.encrypt(file_data)
-    with open("totp.db", "wb") as file:
+    with open("Task 2 - 2FA CLI\data\\totp.db", "wb") as file:
         file.write(encrypted_data)
     print("Database encrypted successfully")
 
@@ -19,10 +19,11 @@ def encrypt_db():
 def decrypt_db():
     with open("Task 2 - 2FA CLI\\temp\key.key", "rb") as key_file:
         key = key_file.read()
+    print(key)
     fernet = Fernet(key)
     with open("Task 2 - 2FA CLI\data\\totp.db", "rb") as file:
         encrypted_data = file.read()
     decrypted_data = fernet.decrypt(encrypted_data)
-    with open("totp.db", "wb") as file:
+    with open("Task 2 - 2FA CLI\data\\totp.db", "wb") as file:
         file.write(decrypted_data)
     print("Database decrypted successfully")
