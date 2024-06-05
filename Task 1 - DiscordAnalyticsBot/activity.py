2ba69052e63d7c864cc11eadb5f555b6c03f8694
Vedant <76871277+theofficialvedantjoshi@users.noreply.github.com>
2024-06-05 12:21:36 +0530

    refactor: firestore credits from env variables

--- Task 1 - DiscordAnalyticsBot/activity.py
+++ Task 1 - DiscordAnalyticsBot/activity.py
@@ -5,12 +5,24 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import io
 
 load_dotenv()
 nlp = SentimentIntensityAnalyzer()
-cred = credentials.Certificate(
-    "D:\CODE\CRUx-Round3-Tasks\stattron-a881c-firebase-adminsdk-fwiky-b0a560b4ca.json"
-)
+creds = {
+    "type": os.getenv("type"),
+    "project_id": os.getenv("project_id"),
+    "private_key_id": os.getenv("private_key_id"),
+    "private_key": os.getenv("private_key"),
+    "client_email": os.getenv("client_email"),
+    "client_id": os.getenv("client_id"),
+    "auth_uri": os.getenv("auth_uri"),
+    "token_uri": os.getenv("token_uri"),
+    "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
+    "client_x509_cert_url": os.getenv("client_x509_cert_url"),
+    "universe_domain": os.getenv("universe_domain"),
+}
+cred = credentials.Certificate(creds)
 firebase_admin.initialize_app(cred)
 
 
